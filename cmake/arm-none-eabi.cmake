c62433dafd4d5813074e1a02abe5da84258648f0
Marco Peter <marco@peter-net.ch>
2022-02-19 15:11:42 +0100

    Also make project compiling in release mode

---
+++ cmake/arm-none-eabi.cmake
@@ -0,0 +1,44 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(TOOLCHAIN arm-none-eabi)
+
+if(WIN32)
+    set(TOOLCHAIN_EXT ".exe" )
+else()
+    set(TOOLCHAIN_EXT "" )
+endif()
+
+set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
+set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/${TOOLCHAIN}/include)
+set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/${TOOLCHAIN}/lib)
+
+set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-gcc${TOOLCHAIN_EXT})
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-g++${TOOLCHAIN_EXT})
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-gcc${TOOLCHAIN_EXT})
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(GENERIC_FLAGS "-mcpu=cortex-m0plus -mthumb -fanalyzer \
+                   -fdata-sections -ffunction-sections -fno-common -fstack-usage \
+                   -Wall -Werror -Wdouble-promotion -Wformat=2 -Wundef \
+                   -Wformat-truncation=2 -Wconversion")
+set(CMAKE_C_FLAGS   "${GENERIC_FLAGS}")
+set(CMAKE_CXX_FLAGS "${GENERIC_FLAGS}")
+set(CMAKE_ASM_FLAGS "${GENERIC_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${GENERIC_FLAGS} -Wl,-Map=$memory.map,--gc-sections")
+
+set(CMAKE_C_FLAGS_DEBUG "-Og -g3")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g3")
+set(CMAKE_ASM_FLAGS_DEBUG "-g3")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
+
+set(CMAKE_C_FLAGS_RELEASE "-Os")
+set(CMAKE_CXX_FLAGS_RELEASE "-Os")
+set(CMAKE_ASM_FLAGS_RELEASE "")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
+
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PREFIX}/${${TOOLCHAIN}})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
